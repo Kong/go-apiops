9329c06e8d6f43d64a237446c3fe0bf7738e3c5f
Thijs Schreijer <thijs@thijsschreijer.nl>
2023-04-06 15:11:20 +0200

    chore(tests): migrate 'merge' tests to ginkgo framework

--- merge/merge_test.go
+++ merge/merge_test.go
@@ -1,66 +1,96 @@
-package merge
+package merge_test
 
 import (
 	"fmt"
 	"strings"
-	"testing"
 
-	"github.com/kong/go-apiops/filebasics"
-	"github.com/stretchr/testify/assert"
+	. "github.com/kong/go-apiops/filebasics"
+	"github.com/kong/go-apiops/merge"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
-func Test_Merge_Files_Good(t *testing.T) {
-	testSet := []struct {
-		files            []string
-		expectedFilename string
-	}{
-		{[]string{
-			"./merge_testfiles/file1.yml",
-			"./merge_testfiles/file2.yml",
-			"./merge_testfiles/file3.yml",
-		}, "test1_expected.json"},
-		{[]string{
-			"./merge_testfiles/file3.yml",
-			"./merge_testfiles/file2.yml",
-			"./merge_testfiles/file1.yml",
-		}, "test2_expected.json"},
-	}
-
-	for _, tdata := range testSet {
-		res, err := Files(tdata.files)
+var _ = Describe("Merge", func() {
+	validateMerge := func(filenames []string, expected string, expectError bool) {
+		GinkgoHelper()
+		res, err := merge.Files(filenames)
 		if err != nil {
-			t.Error(fmt.Sprintf("'%s' didn't expect error: %%w", tdata.files), err)
+			if expectError {
+				// 'expected' is an error string to match
+				Expect(err).To(MatchError(expected))
+			} else {
+				// 'expected' is filename of expected json output, so an error wasn't expected
+				Fail(fmt.Sprintf("'%s' didn't expect error: %s", filenames, err))
+			}
 		} else {
-			expected := filebasics.MustReadFile("./merge_testfiles/" + tdata.expectedFilename)
-			result := filebasics.MustSerialize(res, false)
+			// 'expected' is filename of expected json output
+			if expectError {
+				// 'expected' is an error string to match, but the merge succeeded...
+				Fail(fmt.Sprintf("'%s' expected error: %s", filenames, expected))
+			} else {
+				// 'expected' is filename of expected json output, so an error wasn't expected
+				expectedResult := MustReadFile("./merge_testfiles/" + expected)
+				result := MustSerialize(res, false)
 
-			filebasics.MustWriteSerializedFile("./merge_testfiles/"+
-				strings.Replace(tdata.expectedFilename, "_expected.", "_generated.", -1), res, false)
-			assert.JSONEq(t, string(*expected), string(*result),
-				"'%s': the JSON blobs should be equal", tdata.files)
+				MustWriteSerializedFile("./merge_testfiles/"+
+					strings.Replace(expected, "_expected.", "_generated.", -1), res, false)
+
+				Expect(*result).To(MatchJSON(*expectedResult))
+			}
 		}
 	}
-}
 
-func Test_Merge_Files_Bad(t *testing.T) {
-	testSet := []struct {
-		files       []string
-		errorString string
-	}{
-		{[]string{
-			"./merge_testfiles/file1.yml",
-			"./merge_testfiles/badversion.yml",
-		}, "failed to merge ./merge_testfiles/badversion.yml: files are incompatible; " +
-			"major versions are incompatible; 3.0 and 1.0"},
-		{[]string{
-			"./merge_testfiles/file1.yml",
-			"./merge_testfiles/transform_false.yml",
-		}, "failed to merge ./merge_testfiles/transform_false.yml: files are incompatible; " +
-			"files with '_transform: true' (default) and '_transform: false' are not compatible"},
-	}
+	Describe("merges files", func() {
+		It("attempt 1: original order", func() {
+			// This tests the order of the resulting file, but also the version of the
+			// final file
+			fileList := []string{
+				"./merge_testfiles/file1.yml",
+				"./merge_testfiles/file2.yml",
+				"./merge_testfiles/file3.yml",
+			}
+			expected := "test1_expected.json"
+			expectErr := false
 
-	for _, tdata := range testSet {
-		_, err := Files(tdata.files)
-		assert.EqualError(t, err, tdata.errorString)
-	}
-}
+			validateMerge(fileList, expected, expectErr)
+		})
+
+		It("attempt 2: same files, different order", func() {
+			// This tests the order of the resulting file (different from attempt 1),
+			// but also the version of the final file (same as attempt 1)
+			fileList := []string{
+				"./merge_testfiles/file3.yml",
+				"./merge_testfiles/file2.yml",
+				"./merge_testfiles/file1.yml",
+			}
+			expected := "test2_expected.json"
+			expectErr := false
+
+			validateMerge(fileList, expected, expectErr)
+		})
+
+		It("with incompatible versions errors", func() {
+			fileList := []string{
+				"./merge_testfiles/file1.yml",
+				"./merge_testfiles/badversion.yml",
+			}
+			expected := "failed to merge ./merge_testfiles/badversion.yml: files are incompatible; " +
+				"major versions are incompatible; 3.0 and 1.0"
+			expectErr := true
+
+			validateMerge(fileList, expected, expectErr)
+		})
+
+		It("with incompatible '_transform' errors", func() {
+			fileList := []string{
+				"./merge_testfiles/file1.yml",
+				"./merge_testfiles/transform_false.yml",
+			}
+			expected := "failed to merge ./merge_testfiles/transform_false.yml: files are incompatible; " +
+				"files with '_transform: true' (default) and '_transform: false' are not compatible"
+			expectErr := true
+
+			validateMerge(fileList, expected, expectErr)
+		})
+	})
+})
