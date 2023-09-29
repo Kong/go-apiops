d51793a60b00b0010d5cf50d37957762ad796b6b
Thijs Schreijer <thijs@thijsschreijer.nl>
2023-09-29 21:20:13 +0200

    add tests

--- openapi2kong/openapi2kong_test.go
+++ openapi2kong/openapi2kong_test.go
@@ -39,3 +39,30 @@ func Test_Openapi2kong(t *testing.T) {
 		}
 	}
 }
+
+func Test_Openapi2kong_noID(t *testing.T) {
+	files := []string{
+		"01-names-inferred.yaml",
+		"02-names-set.yaml",
+	}
+
+	for _, file := range files {
+		fileNameIn := file
+		fileNameExpected := strings.TrimSuffix(fileNameIn, ".yaml") + ".expected_noid.json"
+		fileNameOut := strings.TrimSuffix(fileNameIn, ".yaml") + ".generated_noid.json"
+		dataIn, _ := os.ReadFile(fixturePath + fileNameIn)
+		dataOut, err := Convert(dataIn, O2kOptions{
+			Tags:   []string{"OAS3_import", "OAS3file_" + fileNameIn},
+			SkipID: true,
+		})
+		if err != nil {
+			t.Error(fmt.Sprintf("'%s' didn't expect error: %%w", fixturePath+fileNameIn), err)
+		} else {
+			JSONOut, _ := json.MarshalIndent(dataOut, "", "  ")
+			os.WriteFile(fixturePath+fileNameOut, JSONOut, 0o600)
+			JSONExpected, _ := os.ReadFile(fixturePath + fileNameExpected)
+			assert.JSONEq(t, string(JSONExpected), string(JSONOut),
+				"'%s': the JSON blobs should be equal", fixturePath+fileNameIn)
+		}
+	}
+}
