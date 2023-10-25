b8ad323c9a41ab09810025255cbc1128c08b6291
Thijs Schreijer <thijs@thijsschreijer.nl>
2023-10-25 10:49:45 +0200

    chore(dev): add script to quickly cleanup expected files
    Dev utility.
    After a change, the output of many files can change. Tedious to
    update by hand. Once verified the change works as expected, this
    script can update all files at once based on the 'generated'
    versions.

---
+++ openapi2kong/oas3_testfiles/_fixup_expected.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# This script is used to fix up the expected output files in the
+# oas3_testfiles directory.
+# Often when making small changes, a lot of test files are being impacted.
+# This script can be used to fix up the expected output files all at once.
+#
+# 1. make sure the changes have their own tests which succeed, indicating that
+#    the failures in the other files are false-positives now.
+# 2. commit your changes to ensure you can revert the changes by this script!!
+# 3. run the tests with `make test`, to generate all the '*.generated.json' files.
+# 4. run this script to copy the generated files over the expected files.
+# 5. run the tests again with `make test` to verify that the expected files are
+#    now correct.
+
+set -e
+# set -x
+
+if [ -d "openapi2kong" ]; then
+    pushd openapi2kong
+fi
+if [ -d "oas3_testfiles" ]; then
+    pushd oas3_testfiles
+fi
+
+function singlefile() {
+    filename="$1"
+    postfix="$2"
+    destname="${filename%.generated"$postfix".json}.expected$postfix.json"
+    echo "$filename  ->  $destname"
+    cat "$filename" | sed 's/\\u003c/</g' | sed 's/\\u003e/>/g' > "$destname"
+}
+
+# ls -la
+
+for f in ./*.generated.json; do
+    singlefile "$f" ""
+done
