e634f1eaa45f45dd33fc1a048fd2865efc0896dd
Prashansa Kulshrestha <prashansa.kulshrestha@konghq.com>
2025-04-03 12:05:34 +0530

    feat: using env vars for format_version string in deck files

--- merge/merge.go
+++ merge/merge.go
@@ -2,6 +2,8 @@ package merge
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/kong/go-apiops/deckformat"
 	"github.com/kong/go-apiops/filebasics"
@@ -51,6 +53,63 @@ func MustFiles(filenames []string) (result map[string]interface{}, history []int
 	return result, info
 }
 
+// preprocessFormatVersion checks if the _format_version in a file
+// is assigned to an environment variable.
+// If it is, it substitutes the value for further operations.
+func preprocessFormatVersion(data []byte) ([]byte, error) {
+	content := string(data)
+
+	// checking if _format_version is assigned to an env variable
+	formatVersionIndex := strings.Index(content, deckformat.VersionKey)
+	if formatVersionIndex == -1 {
+		return []byte(content), nil
+	}
+
+	templateEnvStart := strings.Index(content[formatVersionIndex:], "${{ env \"")
+	if templateEnvStart == -1 {
+		return []byte(content), nil
+	}
+
+	// absolute position for templateEnvStart
+	templateEnvStart += formatVersionIndex
+
+	// finding env var name
+	envStart := templateEnvStart + len("${{ env \"")
+	envEnd := strings.Index(content[envStart:], "\" }}")
+	if envEnd == -1 {
+		return nil, fmt.Errorf("environment variable quotes not closed")
+	}
+	envVarName := content[envStart : envStart+envEnd]
+
+	value := os.Getenv(envVarName)
+
+	if value == "" {
+		return nil, fmt.Errorf("environment variable '%s' is not set", envVarName)
+	}
+
+	// handling double quoted strings
+	var newValue string
+	var oldValue string
+
+	doubleQuotedString := fmt.Sprintf(`"${{ env "%s" }}"`, envVarName)
+	singleQuotedString := fmt.Sprintf(`'${{ env '%s' }}'`, envVarName)
+
+	if strings.Contains(content, doubleQuotedString) {
+		oldValue = doubleQuotedString
+		newValue = fmt.Sprintf(`"%s"`, value)
+	} else if strings.Contains(content, singleQuotedString) {
+		oldValue = singleQuotedString
+		newValue = fmt.Sprintf(`'%s'`, value)
+	} else {
+		return nil, fmt.Errorf("environment variable '%s' is not templated properly", envVarName)
+	}
+
+	// replace with env var value
+	content = strings.Replace(content, oldValue, newValue, 1)
+
+	return []byte(content), nil
+}
+
 // Files reads and merges files. Will merge all top-level arrays by simply
 // concatenating them. Any other keys will be copied. The files will be processed
 // in order provided. An error will be returned if files are incompatible.
@@ -68,7 +127,17 @@ func Files(filenames []string) (result map[string]interface{}, history []interfa
 		logbasics.Info("merging file", "filename", filename)
 
 		// read the file
-		data, err := filebasics.DeserializeFile(filename)
+		bytedata, err := filebasics.ReadFile(filename)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		preprocessedData, err := preprocessFormatVersion(bytedata)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		data, err := filebasics.Deserialize(preprocessedData)
 		if err != nil {
 			return nil, nil, err
 		}
